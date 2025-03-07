a405ac181a719a4d9b7495a0e4c49298a1f015b1
andywang425 <56548456+andywang425@users.noreply.github.com>
2025-03-07 19:27:38 +0800

    ✨ feat(数据分析): call分析加入日志

--- data-analysis/src/main/java/com/example/telecom/analysis/CallAnalysis.scala
+++ data-analysis/src/main/java/com/example/telecom/analysis/CallAnalysis.scala
@@ -4,7 +4,7 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 
 object CallAnalysis {
-  private val logger = org.slf4j.LoggerFactory.getLogger(this.getClass)
+  private val log = org.slf4j.LoggerFactory.getLogger(this.getClass)
 
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder()
@@ -28,6 +28,8 @@ object CallAnalysis {
         sum($"callDurationMillis").alias("total_duration_millis"),
         count($"callId").alias("total_calls")
       )
+    log.info("按月总通话时长和数量")
+    monthlyCallSummary.show(1024, truncate = false)
 
     // 2. 每月用户通话时长和数量（包含主被叫双方）
     val monthlyCallerCallSummary = callDF.withColumn("userNumber", $"callerNumber")
@@ -37,6 +39,7 @@ object CallAnalysis {
         sum("callDurationMillis").as("caller_total_call_duration") / 2
       )
       .orderBy($"year", $"month", $"userNumber")
+    log.info("每月用户通话时长和数量（主叫）")
     monthlyCallerCallSummary.show(1024, truncate = false)
 
     val monthlyReceiverCallSummary = callDF.withColumn("userNumber", $"receiverNumber")
@@ -46,21 +49,27 @@ object CallAnalysis {
         sum("callDurationMillis").as("receiver_total_call_duration") / 2
       )
       .orderBy($"year", $"month", $"userNumber")
+    log.info("每月用户通话时长和数量（被叫）")
     monthlyReceiverCallSummary.show(1024, truncate = false)
 
     val monthlyUserCallSummary = monthlyCallerCallSummary.join(monthlyReceiverCallSummary, Seq("year", "month", "userNumber"))
       .withColumn("total_call_count", $"caller_call_count" + $"receiver_call_count")
       .withColumn("total_call_duration", $"caller_total_call_duration" + $"receiver_total_call_duration")
+    log.info("每月用户通话时长和数量（主被叫方合并）")
     monthlyUserCallSummary.show(1024, truncate = false)
 
     // 3. 每月通话状态统计
     val monthlyCallStatus = callDF.groupBy($"year", $"month", $"callStatus")
       .agg(count($"callId").alias("call_count"))
+    log.info("每月通话状态统计")
     monthlyCallStatus.show(1024, truncate = false)
 
     // 4. 按月每日小时通话分布统计
     val hourlyCallDistribution = callDF.groupBy($"year", $"month", $"hour")
       .agg(count($"callId").alias("call_count"))
+    log.info("按月每日小时通话分布统计")
     hourlyCallDistribution.show(1024, truncate = false)
+
+    spark.stop()
   }
 }
