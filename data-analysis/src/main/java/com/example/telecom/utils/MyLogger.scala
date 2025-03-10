9c83f18185491fc5977072cf77fb9b19ceaffd7b
andywang425 <56548456+andywang425@users.noreply.github.com>
2025-03-10 13:54:23 +0800

    chore(数据分析): 日志格式调整

--- data-analysis/src/main/java/com/example/telecom/utils/MyLogger.scala
+++ data-analysis/src/main/java/com/example/telecom/utils/MyLogger.scala
@@ -4,11 +4,11 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 trait MyLogger {
-  private val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+  private val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss")
 
   private def log(level: String, content: Any): Unit = {
     val dateTime = LocalDateTime.now.format(formatter)
-    println(s"$dateTime $level ${this.getClass}: $content")
+    println(s"$dateTime $level ${this.getClass.getSimpleName}: $content")
   }
 
   def info(content: Any): Unit = log("INFO", content)
