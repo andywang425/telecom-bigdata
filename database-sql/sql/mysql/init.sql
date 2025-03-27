eec02272ecda3a687821beac8922821206b81f74
andywang425 <56548456+andywang425@users.noreply.github.com>
2025-03-28 00:09:26 +0800

    ✨ feat(sql): 用户表格式调整

--- database-sql/sql/mysql/init.sql
+++ database-sql/sql/mysql/init.sql
@@ -1,1 +1,12 @@
-CREATE DATABASE telecom;
+USE `telecom`;
+
+CREATE TABLE users
+(
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    email      VARCHAR(255) UNIQUE NOT NULL,
+    password   VARCHAR(255)        NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+# INSERT INTO `users` (username, email, password)
+# VALUES ('admin', 'admin@telecom.com', 'admin123');
