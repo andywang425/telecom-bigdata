d46c8ead523086cb6453812a0a08c19912c79202
andywang425 <56548456+andywang425@users.noreply.github.com>
2025-03-31 21:11:02 +0800

    🎈 perf(spring): 尝试优化MonthlyCallSummary的主键，改成内部类

--- spring/src/main/java/com/example/telecom/spring/model/entity/MonthlyCallSummary.java
+++ spring/src/main/java/com/example/telecom/spring/model/entity/MonthlyCallSummary.java
@@ -1,22 +1,32 @@
 package com.example.telecom.spring.model.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EmbeddedId;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
 
 @Data
 @Entity
 @Table(name = "monthly_call_summary")
 public class MonthlyCallSummary {
     @EmbeddedId
-    private MonthlyCallSummaryId id;
+    private Id id;
 
     @Column(name = "total_duration_millis")
     private Long totalDurationMillis;
 
     @Column(name = "total_calls")
     private Long totalCalls;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Embeddable
+    public class Id implements Serializable {
+        private Integer year;
+        private Integer month;
+    }
 }
 
--- spring/src/main/java/com/example/telecom/spring/repository/MonthlyCallSummaryRepository.java
+++ spring/src/main/java/com/example/telecom/spring/repository/MonthlyCallSummaryRepository.java
@@ -1,14 +1,13 @@
 package com.example.telecom.spring.repository;
 
 import com.example.telecom.spring.model.entity.MonthlyCallSummary;
-import com.example.telecom.spring.model.entity.MonthlyCallSummaryId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface MonthlyCallSummaryRepository extends JpaRepository<MonthlyCallSummary, MonthlyCallSummaryId> {
+public interface MonthlyCallSummaryRepository extends JpaRepository<MonthlyCallSummary, MonthlyCallSummary.Id> {
 
     @Query("SELECT m.id.year AS year, SUM(m.totalCalls) AS totalCalls " +
             "FROM MonthlyCallSummary m " +
