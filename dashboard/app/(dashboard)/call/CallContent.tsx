9ac544f4699f04ac2c8ac73033d492ecfc8145fe
andywang425 <56548456+andywang425@users.noreply.github.com>
2025-04-03 20:57:55 +0800

    feat(dashboard): 组件组合，年通话数量组件

--- dashboard/app/(dashboard)/call/CallContent.tsx
+++ dashboard/app/(dashboard)/call/CallContent.tsx
@@ -1,53 +1,16 @@
 'use client';
 import * as React from 'react';
-import YearlyCallsChart from '@/components/YearlyCallsChart';
-import { Box, Card, CardActions, CardContent, Paper } from '@mui/material';
-import Typography from '@mui/material/Typography';
+import { Box, Paper } from '@mui/material';
 import Grid from '@mui/material/Grid';
-import YearRangePicker from '@/components/YearRangePicker';
-import { useEffect } from 'react';
-
-import { CallResponse } from '@/api/types';
-import { getYearlyCalls } from '@/app/(dashboard)/call/actions';
+import YearlyCalls from '@/app/(dashboard)/call/YearlyCalls';
 
 export default function CallContent() {
-  const [startYear, setStartYear] = React.useState<number | null>(2021);
-  const [endYear, setEndYear] = React.useState<number | null>(2024);
-  const [callYearlyData, setCallYearlyData] = React.useState<CallResponse.YearlyCalls[]>([]);
-
-  useEffect(() => {
-    if (startYear && endYear) {
-      getYearlyCalls(startYear, endYear).then(res => {
-        if (res.code === 0) {
-          setCallYearlyData(res.data);
-        }
-      });
-    }
-  }, [startYear, endYear]);
-
   return (
     <Box>
       <Grid container spacing={3}>
         {/* Row 1 */}
         <Grid size={6}>
-          <Card variant="outlined" sx={{ p: 2 }}>
-            <Typography variant="h6" gutterBottom>
-              年通话数量
-            </Typography>
-            <CardContent>
-              <YearlyCallsChart data={callYearlyData} />
-            </CardContent>
-            <CardActions>
-              <YearRangePicker
-                startYear={startYear}
-                endYear={endYear}
-                onChangeAction={(newStartYear, newEndYear) => {
-                  setStartYear(newStartYear);
-                  setEndYear(newEndYear);
-                }}
-              />
-            </CardActions>
-          </Card>
+          <YearlyCalls />
         </Grid>
         <Grid size={6}>
           <Paper sx={{ p: 2, height: 300 }}>Chart B</Paper>
---
+++ dashboard/app/(dashboard)/call/YearlyCalls.tsx
@@ -0,0 +1,48 @@
+import { Card, CardActions, CardContent } from '@mui/material';
+import Typography from '@mui/material/Typography';
+import YearlyCallsChart from '@/components/YearlyCallsChart';
+import YearRangePicker from '@/components/YearRangePicker';
+import * as React from 'react';
+import { CallResponse } from '@/api/types';
+import { useEffect } from 'react';
+import { getYearlyCalls } from '@/app/(dashboard)/call/actions';
+
+export default function YearlyCalls() {
+  const [startYear, setStartYear] = React.useState<number | null>(2021);
+  const [endYear, setEndYear] = React.useState<number | null>(2024);
+  const [callYearlyData, setCallYearlyData] = React.useState<CallResponse.YearlyCalls[]>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await getYearlyCalls(startYear!, endYear!);
+      if (res.code === 0) {
+        setCallYearlyData(res.data);
+      }
+    }
+
+    if (startYear && endYear) {
+      fetchData();
+    }
+  }, [startYear, endYear]);
+
+  return (
+    <Card variant="outlined" sx={{ p: 2 }}>
+      <Typography variant="h6" gutterBottom>
+        年通话数量
+      </Typography>
+      <CardContent>
+        <YearlyCallsChart data={callYearlyData} />
+      </CardContent>
+      <CardActions>
+        <YearRangePicker
+          startYear={startYear}
+          endYear={endYear}
+          onChangeAction={(newStartYear, newEndYear) => {
+            setStartYear(newStartYear);
+            setEndYear(newEndYear);
+          }}
+        />
+      </CardActions>
+    </Card>
+  );
+}
