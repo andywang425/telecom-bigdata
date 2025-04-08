48b1d5263401df1a7b1be8651c261173019de6a0
andywang425 <56548456+andywang425@users.noreply.github.com>
2025-04-08 16:36:32 +0800

    docs(mui): 注释

--- mui-license/main.py
+++ mui-license/main.py
@@ -1,9 +1,9 @@
 import hashlib
 import base64
 
-bs = "Tz0xMjMsRT0zMzc2NjU2MDAwMDAwLFM9cHJlbWl1bSxMTT1wZXJwZXR1YWwsUFY9ZGssS1Y9Mg=="
-
-raw = base64.b64decode(bs).decode()
+# Format: O=${orderNumber},E=${expiryTimestamp},S=${planScope},LM=${licenseModel},PV=${planVersion},KV=2
+# O=订单号（随便填）,E=过期时间（毫秒时间戳）,S=范围（填最大的premium）,LM=许可证模式（填最大的perpetual永久）,PV=计划模式（乱填即可）,KV=密钥版本（必须为2）
+raw = 'O=123,E=3376656000000,S=premium,LM=perpetual,PV=dk,KV=2'
 
 base64_encoded_str = base64.b64encode(raw.encode()).decode()
 
